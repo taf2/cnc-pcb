19165ede137ef7d26d82612ab27950e2fec9879f
Todd Fisher <todd.fisher@gmail.com>
2024-04-27 21:34:43 +0100

    step4 alignment

--- scripts/gen.all.sh
+++ scripts/gen.all.sh
@@ -1,12 +1,20 @@
 /usr/bin/ruby scripts/post-makera.rb flatcam/step1-top-iso.nc T2
 
-/usr/bin/ruby scripts/post-makera.rb flatcam/step2-top-pads.nc T5 0.0000=0.012
+/usr/bin/ruby scripts/post-makera.rb flatcam/step2-top-pads.nc T5 0.1366=0.012
 
-# we know 003 works well, going to test 002
 /usr/bin/ruby scripts/post-laser-makera.rb flatcam/step3-top-silk.nc 003
 
+/usr/bin/ruby scripts/post-makera.rb flatcam/step4-align.nc T3
+/usr/bin/ruby scripts/post-makera.rb flatcam/manual-align.nc T3
 
-/usr/bin/ruby scripts/combine.rb cut/step4-final.nc flatcam/step4-small-holes.nc:T6 flatcam/step4-big-holes.nc:T1 flatcam/step4-cutout.nc:T1
+/usr/bin/ruby scripts/post-makera.rb flatcam/step5-bot-iso.nc T2
+
+/usr/bin/ruby scripts/post-makera.rb flatcam/step6-bot-pads.nc T5 0.1366=0.012
+
+/usr/bin/ruby scripts/post-makera.rb flatcam/step7a-small-holes.nc T6
+/usr/bin/ruby scripts/post-makera.rb flatcam/step7b-small-holes.nc T6
+
+/usr/bin/ruby scripts/combine.rb cut/step8-final.nc flatcam/step7a-small-holes.nc:T6 flatcam/step7b-big-holes.nc:T1 flatcam/step7c-cutout.nc:T1
 
 
 # generate the last step combined drill holes and cut out
