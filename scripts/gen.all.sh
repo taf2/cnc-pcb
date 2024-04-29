20d518f7113af93c14067905b1d4426709a92d09
Todd Fisher <todd.fisher@gmail.com>
2024-04-29 21:30:55 +0100

    update post processor script for makera machine with all steps

--- scripts/gen.all.sh
+++ scripts/gen.all.sh
@@ -1,21 +1,21 @@
+# T1 is 1mm bit
+# T2 is a vbit 60* 0.1mm tip
+# T3 is 2mm bit
+# T4 is 2.8mm bit
+# T5 is spring bit 2mm Vbit 30&
+# T6 is 0.5mm bit
 /usr/bin/ruby scripts/post-makera.rb flatcam/step1-top-iso.nc T2
 
-/usr/bin/ruby scripts/post-makera.rb flatcam/step2-top-pads.nc T5 0.1366=0.012
+/usr/bin/ruby scripts/post-makera.rb flatcam/step2-top-pads.nc T5 0.0000=0.0120
 
 /usr/bin/ruby scripts/post-laser-makera.rb flatcam/step3-top-silk.nc 003
 
 /usr/bin/ruby scripts/post-makera.rb flatcam/step4-align.nc T3
-/usr/bin/ruby scripts/post-makera.rb flatcam/manual-align.nc T3
 
 /usr/bin/ruby scripts/post-makera.rb flatcam/step5-bot-iso.nc T2
 
-/usr/bin/ruby scripts/post-makera.rb flatcam/step6-bot-pads.nc T5 0.1366=0.012
+/usr/bin/ruby scripts/post-makera.rb flatcam/step6-bot-pads.nc T5 0.0000=0.0120
 
-/usr/bin/ruby scripts/post-makera.rb flatcam/step7a-small-holes.nc T6
-/usr/bin/ruby scripts/post-makera.rb flatcam/step7b-small-holes.nc T6
+/usr/bin/ruby scripts/post-laser-makera.rb flatcam/step7-bot-silk.nc 003
 
-/usr/bin/ruby scripts/combine.rb cut/step8-final.nc flatcam/step7a-small-holes.nc:T6 flatcam/step7b-big-holes.nc:T1 flatcam/step7c-cutout.nc:T1
-
-
-# generate the last step combined drill holes and cut out
-#/usr/bin/ruby scripts/combine.rb cut/combined-last.nc flatcam/vias_cnc.nc:T6 flatcam/holes_cnc.nc:T1 flatcam/oval-holes.nc:T1 flatcam/last-cutout.nc:T1
+/usr/bin/ruby scripts/combine.rb cut/final.nc flatcam/step9-small-holes.nc:T6 flatcam/step8-oval-cutout.nc:T1 flatcam/step10-big-holes.nc:T1 flatcam/step11-cutout.nc:T1
