2ac752ddaf0da5bcbd4d9f470849d54906ce1e34
Todd Fisher <todd.fisher@gmail.com>
2024-05-01 02:36:00 +0100

    add offset x and y for laser

--- scripts/post-laser-makera.rb
+++ scripts/post-laser-makera.rb
@@ -8,11 +8,13 @@ output = []
 state = 'discard'
 input_file = ARGV[0]
 power_level = ARGV[1] || 100 # start with 100 to cutout trace and then 10% to cure mask
+x_offset = ARGV[2].to_f || 0.0 # X offset
+y_offset = ARGV[3].to_f || 0.0 # Y offset
+
 output_file = input_file.gsub(/flatcam/,'cut')
 puts "reading #{input_file.inspect}"
 File.readlines(input_file).each {|line|
   line.strip!
-  puts line
 
   # first thing to locate before buffering
   if line.match?(/^M03.*/)
@@ -32,15 +34,28 @@ File.readlines(input_file).each {|line|
     ]
   end
 
+  # Adjust the laser power level
   if line.match?(/G01 Z\-?[0-9]\..*/)
     line = "G01 M325S#{power_level}"
   end
 
+  # instead of a lifting the tool to Z2 we disable the laser since Z2 is the lift height normally
   if line == 'G00 Z2.0000'
     line = 'G01 M325S0'
   end
 
   if state == 'capture'
+    # If we're dealing with a movement command apply our offsets
+    if ((x_offset != 0.0) || (y_offset != 0.0)) 
+      match_data = line.match(/G0[01].*X([-\d.]+)(.*Y([-\d.]+))?/)
+      #match_data = line.match(/G0[01].*X([-\d.]+).*Y([-\d.]+)/)
+      if match_data
+        x = match_data[1].to_f + x_offset
+        y = match_data[3].to_f + y_offset if match_data[3] 
+        line.gsub!(/X[-\d.]+/, "X#{format('%.4f', x)}")
+        line.gsub!(/Y[-\d.]+/, "Y#{format('%.4f', y)}") if y
+      end
+    end
     output << line
   end
 }
